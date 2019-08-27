783c01acc4b3910db3f610d8037a2f2696910e6e
Leonardo Soares Alves Lopes <leonardo.lopes@memt.local>
2019-08-27 14:39:10 -0300

    Commit inicial

---
+++ api/objects/user.php
@@ -0,0 +1,88 @@
+<?php
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $id;
+    public $username;
+    public $password;
+    public $confirm_password;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // signup user
+    function signup(){
+    
+        if($this->isAlreadyExist()){
+            return false;
+        }
+        if($this->$password !== $this->confirm_password){
+        	return false;
+        }
+        if(strlen($this->$password) < 6){
+        	return false;
+        }
+        // query to insert record
+        $query = "INSERT INTO
+                    teste." . $this->table_name . "
+                SET
+                    username=:username, password=:password";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->username=htmlspecialchars(strip_tags($this->username));
+        $this->password=htmlspecialchars(strip_tags($this->password));
+    
+        // bind values
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":password", $this->password);
+    
+        // execute query
+        if($stmt->execute()){
+            $this->id = $this->conn->lastInsertId();
+            return true;
+        }
+    
+        return false;
+        
+    }
+    // login user
+    function login(){
+        // select all query
+        $query = "SELECT
+                    `id`, `username`, `password`
+                FROM
+                    teste." . $this->table_name . " 
+                WHERE
+                    username='".$this->username."' AND password='".$this->password."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        return $stmt;
+    }
+    function isAlreadyExist(){
+        $query = "SELECT *
+            FROM
+                teste." . $this->table_name . " 
+            WHERE
+                username='".$this->username."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        if($stmt->rowCount() > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
